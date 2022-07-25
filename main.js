ee9cb2027f41d8e1a84032fb668b650fccfba757
Sabina Maksimova <maksimovasabina1@gmail.com>
2022-07-25 15:09:15 +0600

    modal

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-API = 'http://localhost:8000/students';
+API = "http://localhost:8000/students";
 
 let photo = document.querySelector(".photo");
 let name = document.querySelector(".name");
@@ -12,67 +12,48 @@ let button = document.querySelector(".button");
 
 let list = document.querySelector("#products-list");
 
-btnAdd.addEventListener("click", async () => {r 
+let searchVal = "";
 
-    let obj = {
-        photo: photo.value,
-        name: name.value,
-        surname: surname.value,
-        kpi: kpi.value,
-        groups: groups.value,
-        number: namber.value,
-        email: email.value,
-        gitHub: gitHub.value,
-        button: button.value,
-    };
+button.addEventListener("click", async () => {
+  let obj = {
+    photo: photo.value,
+    name: name.value,
+    surname: surname.value,
+    kpi: kpi.value,
+    groups: groups.value,
+    number: number.value,
+    email: email.value,
+    gitHub: gitHub.value,
+    button: button.value,
+  };
 
-
-    if (!obj.photo.trim() ||
-        !obj.name.trim() ||
-        !obj.surname.trim() ||
-        !obj.kpi.trim() ||
-        !obj.groups.trim() ||
-        !obj.number.trim() ||
-        !obj.email.trim() ||
-        !obj.gitHub.trim() ||
-        !obj.button.trim()
-    ) {
-        alert("fill all fields");
-        return;
-    }
-    // запрос для добавления 
-
-    await fetch(API, {
-        method: "POST",
-        body: JSON.stringify(obj),
-        headers: {
-            "Content-type": "application/json; charset=utf-8",
-        },
-    });
-    title.value = "";
-    price.value = "";
-    descr.value = "";
-    image.value = "";
-    render();
+  await fetch(API, {
+    method: "POST",
+    body: JSON.stringify(obj),
+    headers: {
+      "Content-type": "application/json; charset=utf-8",
+    },
+  });
+  photo.value = "";
+  name.value = "";
+  surname.value = "";
+  groups.value = "";
+  render();
 });
 
 async function render() {
-    let products = await fetch(`${API}?q=${searchVal}`)
-        .then((res) => res.json());
-    // console.log(products); 
+  let products = await fetch(`${API}?q=${searchVal}`).then((res) => res.json());
 
-    list.innerHTML = "";
+  list.innerHTML = "";
 
-    products.forEach((element) => {
-        // console.log(element); 
-        let newElem = document.createElement("div");
-        newElem.innerHTML = 
-        `
+  products.forEach((element) => {
+    let newElem = document.createElement("div");
+    newElem.innerHTML = `
         <div class="card" style="width: 18rem;">
             <img src="${element.photo}" class="card-img-top" alt="">
             <div class="card-body">
-                <h5 class="card-title">${element.name}</h5>
-                <h5 class="card-title">${element.surname}</h5>
+                <h5 class="card-photo">${element.name}</h5>
+                <h5 class="card-photo">${element.surname}</h5>
                 <p class="card-text">${element.kpi}</p>
             </div>
             <ul class="list-group list-group-flush">
@@ -82,12 +63,95 @@ async function render() {
                 <li class="list-group-item">${element.gitHub}</li>
             </ul>
             <div class="card-body">
-                <a href="#" class="card-link">Delete</a>
-                <a href="#" class="card-link">Edit</a>
+                <a href="#" class="card-link btn-delete"  id=${element.id} >Delete</a>
+                <a href="#" class="card-link btn-edit"  id=${element.id}>Edit</a>
             </div>
         </div>`;
 
-        list.append(newElem);
-    });
-};
+    list.append(newElem);
+  });
+}
 render();
+
+let editPhoto = document.querySelector(".edit-photo");
+let editName = document.querySelector(".edit-name");
+let editSurname = document.querySelector(".edit-surname");
+let editKpi = document.querySelector(".edit-kpi");
+let editGroups = document.querySelector(".edit-groups");
+let editNumber = document.querySelector(".edit-number");
+let editEmail = document.querySelector(".edit-email");
+let editGitHub = document.querySelector(".edit-gitHub");
+let btnSaveEdit = document.querySelector("#btn-save-edit");
+
+document.addEventListener("click", async (e) => {
+  if (e.target.classList.contains("btn-delete")) {
+    let answer = confirm("are you sure?");
+    let id = e.target.id;
+
+    if (answer) {
+      await fetch(`${API}/${id}`, {
+        method: "DELETE",
+      });
+      render();
+    }
+  }
+});
+
+// searchInp.addEventListener("input", () => {
+//   searchVal = searchInp.value;
+//   console.log(searchInp.value);
+//   render();
+// });
+
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-edit")) {
+    let id = e.target.id;
+    fetch(`${API}/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        editPhoto.value = data.photo;
+        editName.value = data.name;
+        editSurname.value = data.surname;
+        editKpi.value = data.kpi;
+        editGroups.value = data.groups;
+        editNumber.value = data.number;
+        editEmail.value = data.email;
+        editGitHub.value = data.gitHub;
+        btnSaveEdit.setAttribute("id", data.id);
+      });
+  }
+});
+
+btnSaveEdit.addEventListener("click", (e) => {
+  let id = e.target.id;
+  let photo2 = editPhoto.value;
+  let name2 = editName.value;
+  let surname2 = editSurname.value;
+  let groups2 = editGroups.value;
+  let number2 = editNumber.value;
+  let email2 = editEmail.value;
+  let gitHub2 = editGitHub.value;
+
+  let edittedProduct = {
+    photo: photo2,
+    name: name2,
+    surname: surname2,
+    groups: groups2,
+    number: number2,
+    email: email2,
+    gitHub: gitHub2,
+  };
+  saveEdit(edittedProduct, id);
+});
+
+function saveEdit(edittedProduct, id) {
+  fetch(`${API}/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(edittedProduct),
+  }).then(() => {
+    render();
+  });
+}
